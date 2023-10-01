b8747375ecd3bc2da365960cd97e02fa6161e84b
designermanjeets <designermanjeets@gmail.com>
2023-10-01 08:03:57 +0530

    Initial Commit

---
+++ starterkit/_keenthemes/assets/webpack/plugins/custom/draggable/draggable.bundle.js
@@ -0,0 +1,56 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./webpack/plugins/custom/draggable/draggable.js":
+/*!*******************************************************!*\
+  !*** ./webpack/plugins/custom/draggable/draggable.js ***!
+  \*******************************************************/
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("// Draggable - a lightweight, responsive, modern drag & drop library: https://shopify.github.io/draggable/\n\n__webpack_require__(Object(function webpackMissingModule() { var e = new Error(\"Cannot find module '@shopify/draggable/lib/draggable.bundle.js'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }()));\n__webpack_require__(Object(function webpackMissingModule() { var e = new Error(\"Cannot find module '@shopify/draggable/lib/draggable.bundle.legacy.js'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }()));\n__webpack_require__(Object(function webpackMissingModule() { var e = new Error(\"Cannot find module '@shopify/draggable/lib/draggable.js'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }()));\nwindow.Sortable = __webpack_require__(Object(function webpackMissingModule() { var e = new Error(\"Cannot find module '@shopify/draggable/lib/sortable.js'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }()));\nwindow.Droppable = __webpack_require__(Object(function webpackMissingModule() { var e = new Error(\"Cannot find module '@shopify/draggable/lib/droppable.js'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }()));\nwindow.Swappable = __webpack_require__(Object(function webpackMissingModule() { var e = new Error(\"Cannot find module '@shopify/draggable/lib/swappable.js'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }()));\n__webpack_require__(Object(function webpackMissingModule() { var e = new Error(\"Cannot find module '@shopify/draggable/lib/plugins.js'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }()));\n__webpack_require__(Object(function webpackMissingModule() { var e = new Error(\"Cannot find module '@shopify/draggable/lib/plugins/collidable.js'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }()));\n__webpack_require__(Object(function webpackMissingModule() { var e = new Error(\"Cannot find module '@shopify/draggable/lib/plugins/resize-mirror.js'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }()));\n__webpack_require__(Object(function webpackMissingModule() { var e = new Error(\"Cannot find module '@shopify/draggable/lib/plugins/snappable.js'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }()));\n__webpack_require__(Object(function webpackMissingModule() { var e = new Error(\"Cannot find module '@shopify/draggable/lib/plugins/swap-animation.js'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }()));\n\n\n//# sourceURL=webpack://keenthemes/./webpack/plugins/custom/draggable/draggable.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./webpack/plugins/custom/draggable/draggable.js");
+/******/ 	
+/******/ })()
+;
